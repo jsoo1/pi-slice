7fe105b73620a1f4f39f162ee425944c2e7d2094
John Soo <jsoo1@asu.edu>
2016-09-11 17:19:32 -0700

    (clean) Remove demonstrative node files

--- node-service/middleware.js
+++
@@ -1,9 +0,0 @@
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-
-module.exports = function (app, express) {
-  app.use(morgan('dev'))
-  app.use(bodyParser.json())
-
-  return app
-}
--- node-service/routes.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (app, express) {
-  app.use('/', express.static('client'))
-  
-  return app
-}

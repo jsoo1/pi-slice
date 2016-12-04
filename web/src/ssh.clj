5327bd10f0cf8ee25eaebb0ce9672f885423da79
John Soo <jsoo1@asu.edu>
2016-12-04 10:43:57 -0800

    (WIP) Still more tweaking of ssh file. Forget atoms, use channels and expose in/out.

--- web/src/ssh.clj
+++ web/src/ssh.clj
@@ -1,15 +1,24 @@
 (ns pi-ssh
   (:require [clj-ssh.ssh :as jsch]
+            [clojure.core.async :as a :refer [chan thread >! <! >!! <!!
+                                              close! timeout go go-loop]]
             [clojure.java.io :as io]
             [taoensso.timbre :as timbre
              :refer [log trace debug info warn error fatal report logf tracef
                      debugf infof warnf errorf fatalf reportf spy get-env]]
-            [clojure.core.async :as a :refer [chan thread >! <! >!! <!!
-                                              close! timeout]]))
-;; {(atom jsch.Agent) #{(atom jsch.Session)}}
-(def agents (atom {}))
-;; {(atom jsch.Session) #{(atom jsch.channel)}}
-(def connections (atom {}))
+            [util :as u]))
+
+;; SSH state tracking atoms
+;; TODO Determine if really needed as the implementation might be out of scope.
+;; Else, implement them.
+;; (def agents
+;;   ^{:doc "{jsch/Agent #{jsch/Session}}
+;; A map of jsch/Agents to a set of jsch/Sessions using the associated agent."}
+;;   (atom {}))
+;; (def connections
+;;   ^{:doc "{jsch/Session #{jsch/channel}}
+;; A map of jsch/Sessions to a set of jsch/Channels using the associated session."}
+;;   (atom {}))
 
 (defn eval-config
   "conf-path->
@@ -18,7 +27,8 @@
       :id-options {:private-key-path String :passphrase ByteArray}
       :session-options {:port int
                         :username String
-                        :strict-host-key-checking keyword}}"
+                        :strict-host-key-checking keyword
+                        :server-alive-interval int}}"
   [conf-path]
   (->> conf-path
        (str (System/getProperty "user.dir"))
@@ -27,74 +37,67 @@
        (#(do (debug (str "Read config: " conf-path)) %))
        (eval)))
 
-(defn fnil-set
-  "x->(coll of coll-type)->(conj coll x)
-  Return a function which either creates a set containing x or adds x to an existing set"
-  [x]
-  #(if %
-     (conj % x)
-     #{x}))
-
 (defn session-from
-  "conf map->(atom jsch.Session)"
+  "conf map->jsch.Session"
   [{:keys [agent-options id-options host session-options]}]
-  (let [agent (atom (jsch/ssh-agent agent-options))]
-    (swap! agent #(do (jsch/add-identity % id-options) %))
-    (let [session (atom (jsch/session @agent host session-options))]
-      ;; Update the state of the agents
-      ;; TODO Make this actually have distinct sessions.
-      (swap! agents #(update % agent (fnil-set session)))
-      session)))
+  (let [agent (doto (jsch/ssh-agent #dbg agent-options)
+                (jsch/add-identity #dbg id-options))
+        ;; Unfortunately, clj-ssh does not accept server-alive-interval
+        ;; as an option. So we pull all the stuff out here to provide a
+        ;; unified API. This also allows reasonable default values...
+        {port ::port
+         username ::username
+         strict-host-key-checking ::strict-host-key-checking
+         server-alive-interval ::server-alive-interval} session-options]
+    (doto (jsch/session agent
+                        host
+                        {::port (or port 22)
+                         ::username (or username "")
+                         ::strict-host-key-checking (or strict-host-key-checking
+                                                       true)})
+      (.setServerAliveInterval (or server-alive-interval 30000)))))
+
+(defn print-do
+  [x]
+  (do (clojure.pprint/pprint x) x))
 
 (defn run-in-channel
-  "Takes a session and a continuation: (fn \"handler\" [channel-atom in out])
+  "Takes a session and a continuation: (fn \"handler\" [in out])
   to execute with the channel open."
   ([session handler] (run-in-channel session handler {}))
   ([session handler {:keys [server-alive-interval in-stream out-stream]}]
-   (swap! session
-          (fn [sesh]
-            (do
-              (doto sesh
-                (.setServerAliveInterval (or server-alive-interval 30000)))
-              (jsch/with-connection sesh
-                (let [channel (atom (jsch/open-channel sesh 'shell))
-                      in (or in-stream (.getInputStream @channel))
-                      out (or out-stream (.getOutputStream @channel))]
-                  (jsch/with-channel-connection @channel
-                    (debug "Channel connected, proceeding")
-                    ;; Update the state of the connections
-                    (swap! connections #(update % session (fnil-set channel)))
-                    (handler channel in out)))))))))
+   (do
+     (doto session
+       )
+     (jsch/with-connection session
+       (let [channel (atom (jsch/open-channel session 'shell))
+             in (or in-stream (.getInputStream @channel))
+             out (or out-stream (.getOutputStream @channel))]
+         (jsch/with-channel-connection @channel
+           (debug "Channel connected, proceeding")
+           ;; Update the state of the connections
+           (handler in out)))))))
 
-(defn buffer->str
-  "Array<Byte>->String
-  Transforms a java array of bytes into a String."
-  [buffer]
-  (->> buffer
-       (seq)
-       (filter pos?)
-       (map char)
-       (clojure.string/join)))
+(defn slurp-stream
+  "[stream, (optional)size]->java Array<Byte> of length size
+  Creates a realized buffer array of all input from a stream."
+  ([stream] (slurp-stream stream 1024))
+  ([stream size]
+   (let [buffer (make-array Byte/TYPE size)]
+     (do #dbg (.read stream buffer 0 (.available stream))
+        (seq buffer)))))
 
 (defn chandler
-  "Work with a (atom channel), and its streams.
-  (in is where ssh output goes, and out is where to put ssh input,
-  in keeping with jsch \"conventions\")"
-  [ch in out]
-  (let [buffer (make-array Byte/TYPE 1024)
-        num-chars (.read in buffer)
-        connected (jsch/connected-channel? @ch)]
-    (println (buffer->str buffer))
-    (debug "Collected stdout, channel connected:\t" connected)
-    (if connected
-      (let [input (.getBytes "ls")]
-        (let [new-ch (doto out
-                       (.flush)
-                       (.write input 0 2))
-              buff (make-array Byte/TYPE 1024)
-              num-cs (.read in buff)]
-          (println (buffer->str buff)))
-        (debug "Channel disconnected")))))
+  "Work with a shell channel streams.
+  Occurs within with-channel-connection, hopefully.
+  <PipedInputStream>in and <PipedOutputStream>out.
+
+  A note on the streams: in is where ssh output goes,
+  and out is where to put ssh input, in keeping with java io."
+  [in out]
+  (let [in-chan (chan)
+        out-chan (chan)]
+    (go-loop [])))
 
 (-> "/conf/ssh.edn"
     (eval-config)
@@ -106,15 +109,3 @@
     (println "agents:\t")
     (clojure.pprint/pprint @agents))
 
-;; (let [dir (System/getProperty "user.dir")
-      ;; buffer (make-array Byte/TYPE 1024)]
-  ;; (with-open [in (io/input-stream (str dir "/in.log"))
-              ;; out (io/output-stream (str dir "/out.log"))]
-    ;; (let [num-chars (.read in buffer)]
-      ;; ( ->> buffer
-       ;; (seq)
-       ;; (filter #(not (zero? %)))
-       ;; (map char)
-       ;; (clojure.string/join))
-      ;; (.write out buffer 0 num-chars)))
-  ;; (slurp (str dir "/out.log")))

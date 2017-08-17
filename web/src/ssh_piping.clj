ed1182e637c1a4c738588c002ccc9e3dd09109be
John Soo <jsoo1@asu.edu>
2017-08-17 11:38:04 -0700

    Add some WIP piping

--- web/src/ssh_piping.clj
+++ web/src/ssh_piping.clj
@@ -5,35 +5,52 @@
             [taoensso.timbre :as t :refer [debug]]
             [util :as u]))
 
-(let [{in :pi-ssh/in-stream out :pi-ssh/out-stream}
-      (<!! (go (->> (pi-ssh/eval-config "/conf/ssh_aws.edn")
-                    (s/conform :pi-ssh/conf-map)
-                    (#(do (debug "Read configuration:" %) %))
-                    (pi-ssh/session-from)
-                    (pi-ssh/shell-streams)
-                    (#(do (debug "streams" %) %)))))
-      ssh-out-c (a/chan)
-      user-in-c (a/chan)]
 
-  (let [buffer (make-array Byte/TYPE 1024)]
-    (go-loop []
-      (when (pos? (.available in))
-        (do (.read buffer 0 (.available in))
-            (>! ssh-out-c (u/buffer->str buffer))))
-      (recur)))
+(def chans (let [{:keys [:pi-ssh/in-stream :pi-ssh/out-stream]}
+                 (<!! (go (->> (pi-ssh/eval-config "/conf/ssh_aws.edn")
+                               (s/conform :pi-ssh/conf-map)
+                               (#(do (debug "Read configuration:" %) %))
+                               (pi-ssh/session-from)
+                               (pi-ssh/shell-streams))))
 
-  (go-loop [line nil]
-    (when line
-      (doto out
-        (.flush)
-        (.write (.getBytes line) 0 (count line))))
-    (recur (<! user-in-c)))
+                 ssh-out-chan (a/chan)
+                 user-in-chan (a/chan)
 
-  (go-loop [line nil]
-    (when line (debug line))
-    (recur (<! ssh-out-c)))
+                 user-in-pipe-chan (go-loop [line (<! user-in-chan)]
+                                     (condp = line
+                                       ":q" :pi-ssh/quit
+                                       nil :pi-ssh/closed
+                                       (do (doto out-stream
+                                             (.flush)
+                                             (.write (.getBytes line) 0 (count line)))
+                                           (recur (<! user-in-chan)))))
 
-  (go-loop [line (read-line)]
-    (debug line)
-    (>! user-in-c line)
-    (recur (read-line))))
+                 user-in-status-chan        (go-loop [line (read-line)]
+                                              (debug line)
+                                              (condp = line
+                                                ":q" (do (a/close! user-in-chan)
+                                                         :pi-ssh/quit)
+                                                (recur (read-line))))
+
+                 ssh-out-pipe-chan (let [buffer (make-array Byte/TYPE 1024)]
+                                     (go-loop []
+                                       (condp = (.available in-stream)
+                                         0 :pi-ssh/closed
+                                         (do (.read buffer 0 (.available in-stream))
+                                             (debug (u/buffer->str buffer))
+                                             (>! ssh-out-chan (u/buffer->str buffer))
+                                             (recur)))))
+
+                 ssh-display-chan (go-loop [output (<! ssh-out-chan)]
+                                    (condp = output
+                                      :pi-ssh/closed :pi-ssh/closed
+                                      nil :pi-ssh/closed
+                                      (do (debug output)
+                                          (recur (<! ssh-out-chan)))))]
+
+             {:out ssh-out-chan
+              :in user-in-chan
+              :in-status user-in-status-chan
+              :in-pipe user-in-pipe-chan
+              :out-pipe ssh-out-pipe-chan
+              :display ssh-display-chan}))
